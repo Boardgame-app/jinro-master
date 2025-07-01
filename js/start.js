c18b70d5cac34cca439ac8d6c10ae470daea6f9c
Boardgame-app <eternal_centuria@yahoo.co.jp>
2025-07-02 00:04:02 +0900

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -1,5 +1,6 @@
 document.getElementById("start").addEventListener("click", function() {
   document.getElementById("title-image").style.display = "none";
   document.getElementById("start").style.display = "none";
+    document.getElementById("copyright").style.display = "none";
   document.getElementById("content").style.display = "block";
 });
