965c8fc2a6ff434c83fa55c348e14f9ed6fffff2
Boardgame-app <eternal_centuria@yahoo.co.jp>
2025-07-05 10:45:24 +0900

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -1,19 +1,6 @@
-document.getElementById("startscreen").addEventListener("click", function() {
+function start() {
   document.getElementById("title-image").style.display = "none";
   document.getElementById("start").style.display = "none";
   document.getElementById("copyright").style.display = "none";
   document.getElementById("content").style.display = "block";
-});
-
-<script>
-  function adjustScale() {
-    const content = document.querySelector('.content');
-    const scaleX = window.innerWidth / 800;
-    const scaleY = window.innerHeight / 600;
-    const scale = Math.min(scaleX, scaleY);  // 画面に収まる最大サイズに調整
-    content.style.setProperty('--scale', scale);
-  }
-
-  window.addEventListener('resize', adjustScale);
-  window.addEventListener('load', adjustScale);
-</script>
+}
